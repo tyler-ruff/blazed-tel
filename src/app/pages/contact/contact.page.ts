2e12a67afb1908049429104f0dc66c9add047d23
Tyler Ruff <hello@blazed.space>
2023-07-28 07:31:51 -0400

    astro

--- src/app/pages/contact/contact.page.ts
+++
@@ -1,6 +0,0 @@
-import { Page } from 'fire-ng/lib/interfaces/page';
-
-export const page = {
-    title: "Contact",
-    description: "Contact Blazed Telecom."
-} as Page;
--- src/app/pages/success/success.page.ts
+++
@@ -1,6 +0,0 @@
-import { Page } from 'fire-ng/lib/interfaces/page';
-
-export const page = {
-    title: "Success",
-    description: "Your message has been successfully sent."
-} as Page;
